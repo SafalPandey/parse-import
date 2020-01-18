430fb98c13d70f3d2790ca1d806b3a88aff70ae0
Safal Raj Pandey <safal.pandey.sp@gmail.com>
2020-01-18 21:16:26 +0545

    Add maps for language <-> constants

---
+++ core/constants.go
@@ -0,0 +1,21 @@
+package core
+
+var ImportPatternMap = map[string]string{
+	"ts": `(?sm)^import (?P<name>.+)\s*from\s+(?P<module>\S+)`,
+	"py": `(?sm)(?:^import\s+(?P<module>\S+))|(?:^from (?P<module>.+)\s*import\s+(?P<name>\S+))`,
+}
+
+var ExtensionMap = map[string][]string{
+	"ts": []string{"/index.ts", "/index.tsx", "/index.js", "", ".tsx", ".ts", ".js", ".json"},
+	"py": []string{".py"},
+}
+
+var SplitCharMap = map[string]byte{
+	"ts": ';',
+	"py": '\n',
+}
+
+var PathDelimiterMap = map[string]string{
+	"ts": "/",
+	"py": ".",
+}
