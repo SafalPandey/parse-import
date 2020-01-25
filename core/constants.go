62a08cefdec43fc0bb703f41fda338c5ce284f5c
Safal Raj Pandey <safal.pandey.sp@gmail.com>
2020-01-25 13:56:09 +0545

    Update import regex for ts to support 'require' imports

--- core/constants.go
+++ core/constants.go
@@ -1,7 +1,7 @@
 package core
 
 var ImportPatternMap = map[string]string{
-	"ts": `(?sm)^import (?P<name>.+)\s*from\s+(?P<module>\S+)`,
+	"ts": `(?sm)(?:^import (?P<name>.+)\s*from\s+(?P<module>\S+))|(?:^(?:const|let|var)\s*(?P<name>\S+)\s*=\s*require\((?P<module>\S+)\))`,
 	"py": `(?sm)(?:^import\s+(?P<module>\S+))|(?:^from (?P<module>.+)\s*import\s+(?P<name>\S+))`,
 }
 
