b55726cd0a244b9b6c13dd8053d99af2aacf1727
Safal Raj Pandey <safal.pandey.sp@gmail.com>
2020-01-25 14:00:03 +0545

    Create a map to find and update Localdirs and BaseDirAbsPathMap global variables

--- core/constants.go
+++ core/constants.go
@@ -1,5 +1,14 @@
 package core
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"path"
+	"strings"
+
+	"../utils"
+)
+
 var ImportPatternMap = map[string]string{
 	"ts": `(?sm)(?:^import (?P<name>.+)\s*from\s+(?P<module>\S+))|(?:^(?:const|let|var)\s*(?P<name>\S+)\s*=\s*require\((?P<module>\S+)\))`,
 	"py": `(?sm)(?:^import\s+(?P<module>\S+))|(?:^from (?P<module>.+)\s*import\s+(?P<name>\S+))`,
@@ -18,4 +27,23 @@ var SplitCharMap = map[string]byte{
 var PathDelimiterMap = map[string]string{
 	"ts": "/",
 	"py": ".",
+}
+
+var FindLocalDirsMap = map[string]func(string){
+	"ts": func(tsconfigPath string) {
+		data, err := ioutil.ReadFile(tsconfigPath)
+		utils.CheckError(err)
+
+		var obj map[string]map[string]string
+		json.Unmarshal(data, &obj)
+
+		baseDirAbsPath := path.Join(path.Dir(tsconfigPath), obj["compilerOptions"]["baseUrl"])
+
+		files, _ := ioutil.ReadDir(baseDirAbsPath)
+		for _, file := range files {
+			localDir := strings.Split(file.Name(), ".")[0]
+			LocalDirs = append(LocalDirs, localDir)
+			BaseDirAbsPathMap[localDir] = baseDirAbsPath
+		}
+	},
 }
