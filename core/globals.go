ba48f472bb3ffface25c3a393d4ce45e3b49a50c
Safal Raj Pandey <safal.pandey.sp@gmail.com>
2020-01-25 14:04:42 +0545

    Make FindLocalDirs configurable using language and upgrade BaseDirAbsPath to a map

--- core/globals.go
+++ core/globals.go
@@ -1,46 +1,26 @@
 package core
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"path"
 	"regexp"
-	"strings"
-
-	"../utils"
 )
 
 // LocalDirs global var
 var LocalDirs []string
 
 // BaseDirAbsPath global var
-var BaseDirAbsPath string
+var BaseDirAbsPathMap = make(map[string]string)
 
 var SplitChar byte
 var Language string
 var Extensions []string
 var PathDelimiter string
 var ImportPattern *regexp.Regexp
+var FindLocalDirs func(string)
 
 func ComputeConstants() {
 	SplitChar = SplitCharMap[Language]
 	Extensions = ExtensionMap[Language]
 	PathDelimiter = PathDelimiterMap[Language]
+	FindLocalDirs = FindLocalDirsMap[Language]
 	ImportPattern = regexp.MustCompile(ImportPatternMap[Language])
-}
-
-// FindLocalDirs util
-func FindLocalDirs(tsconfigPath string) {
-	data, err := ioutil.ReadFile(tsconfigPath)
-	utils.CheckError(err)
-
-	var obj map[string]map[string]string
-	json.Unmarshal(data, &obj)
-
-	BaseDirAbsPath = path.Join(path.Dir(tsconfigPath), obj["compilerOptions"]["baseUrl"])
-
-	files, _ := ioutil.ReadDir(BaseDirAbsPath)
-	for _, file := range files {
-		LocalDirs = append(LocalDirs, strings.Split(file.Name(), ".")[0])
-	}
 }
