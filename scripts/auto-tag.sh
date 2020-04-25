c78f5cc9e79b7cdf6e7b49d479483a18ee40d9d3
Safal Raj Pandey <safal.pandey.sp@gmail.com>
2020-04-25 16:56:59 +0545

    Fix auto tag variable assignment

--- scripts/auto-tag.sh
+++ scripts/auto-tag.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-last_master_tag=$(git tag --sort=-creatordate | grep -P "^\d+.\d+.\d+$" | head -n 1)
+last_master_tag=`git tag --sort=-creatordate | grep -P "^\d+.\d+.\d+$" | head -n 1`
 echo -e "\nLast tag: $last_master_tag\n"
 
-bumped_tag=$(semver bump patch "$last_master_tag")
+bumped_tag=`semver bump patch "$last_master_tag"`
 echo -e "\nBumped tag: $bumped_tag\n"
 
 git tag "$bumped_tag"
