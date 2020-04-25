22a00efc9e6176b8fbb969796f27bd05f44950de
Safal Raj Pandey <safal.pandey.sp@gmail.com>
2020-04-25 16:50:29 +0545

    Add logs in auto-tag.sh

--- scripts/auto-tag.sh
+++ scripts/auto-tag.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+set -e
+
 last_master_tag=$(git tag --sort=-creatordate | grep -P "^\d+.\d+.\d+$" | head -n 1)
+echo -e "\nLast tag: $last_master_tag\n"
+
 bumped_tag=$(semver bump patch "$last_master_tag")
+echo -e "\nBumped tag: $bumped_tag\n"
 
 git tag "$bumped_tag"
 git push origin HEAD --tags
